ee26983c72cc9d32f47b80c079d4274ba71cfcc4
Stugr <Warintorn.ni@ku.th>
2025-01-09 14:30:39 +0700

    ส่งงานครั้งที่ 1

--- app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
@@ -1,5 +1,7 @@
 package scisrc.mobiledev.firstuiassignment;
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 public class RegisterActivity : AppCompatActivity() {
 
     lateinit var backToMainPageBtn: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +26,11 @@ public class RegisterActivity : AppCompatActivity() {
 
         backToMainPageBtn = findViewById(R.id.registBackToMainBtn)
 
+
         backToMainPageBtn.setOnClickListener() {
             finish()
         }
+
+
     }
 }
